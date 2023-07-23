67436fcf9a0006a4397676319614cf3bc4e6fe59
Helen Manders-Jones <realhelenmjones@gmail.com>
2023-07-23 16:02:51 +0100

    first commit

---
+++ packages/nbn-react-components/src/components/index.ts
@@ -0,0 +1,8 @@
+// export * from './Button'
+export { default as Button} from './Button'
+//same as:
+//import Button from './Button';
+//export default Button;
+
+// export { default as Link} from './Link'
+// export * from './Link'
