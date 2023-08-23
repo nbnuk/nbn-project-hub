b117e0eb60833ccf086963ccef3365331e55ce87
Helen Manders-Jones <realhelenmjones@gmail.com>
2023-08-23 21:20:14 +0100

    Created storybook section

--- packages/nbn-react-components/src/components/OccurrenceList/OccurrenceList.stories.ts
+++ packages/nbn-react-components/src/components/OccurrenceList/OccurrenceList.stories.ts
@@ -4,7 +4,7 @@ import { OccurrenceList } from './OccurrenceList';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Example/OccurrenceList',
+  title: 'Occurrences/OccurrenceList',
   component: OccurrenceList,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
