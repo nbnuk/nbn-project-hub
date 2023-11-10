03ac65ba3592af56e22fb2aa2e1aac645471e569
Helen Manders-Jones <h.manders.jones@nbn.org.uk>
2023-11-10 13:18:16 +0000

    removed Button component

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Button} from '@nbnuk/nbn-react-components'
+import {SimpleOccurrenceSearch} from '@nbnuk/nbn-react-components'
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button label="Goddam"/>
+        <SimpleOccurrenceSearch />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
