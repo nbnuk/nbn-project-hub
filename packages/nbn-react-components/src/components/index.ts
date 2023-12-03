f700cb8799e10eb1c5d9bb22003527edfdfd2d16
Helen Manders-Jones <h.manders.jones@nbn.org.uk>
2023-12-03 10:48:17 +0000

    removed deleted Button export

--- packages/nbn-react-components/src/components/index.ts
+++ packages/nbn-react-components/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Button} from './Button'
 export { default as TemplateDataFetchingComponent} from './TemplateDataFetchingComponent'
 export { default as Map} from './Map'
 export { default as MapTitle} from './MapTitle';
