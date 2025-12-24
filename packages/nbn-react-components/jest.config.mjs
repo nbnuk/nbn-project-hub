6ac0f144ff11461824f5a5c95e40612b1107be08
Daud Sweetland <D.Sweetland@nbn.org.uk>
2025-12-24 09:57:15 +0000

    Initial upgrade to all packages, and removal of CRA which is now EOSL

--- packages/nbn-react-components/jest.config.mjs
+++ packages/nbn-react-components/jest.config.mjs
@@ -2,13 +2,19 @@ export default {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['@testing-library/jest-dom'],
     transform: {
-        '^.+\\.(t|j)sx?$': ['@swc/jest'],
+        '^.+\\.(t|j)sx?$': [
+            '@swc/jest',
+            {
+                jsc: {
+                    parser: { syntax: 'typescript', tsx: true },
+                    transform: { react: { runtime: 'automatic' } },
+                    target: 'es2022',
+                },
+                module: { type: 'commonjs' },
+            },
+        ],
     },
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '\\.test\\.js$',
-        '\\.spec\\.js$',
-    ],
+    testPathIgnorePatterns: ['/node_modules/', '\\.test\\.js$', '\\.spec\\.js$'],
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
